ee6bbbe5da2ac79e875f5dc8d49515640c93339a
Bryanrobu <1211898@student.roc-nijmegen.nl>
2024-09-18 16:44:14 +0200

    s

---
+++ UserInput/src/chickens01/Chickens01.java
@@ -0,0 +1,32 @@
+
+package chickens01;
+
+import java.util.Scanner;
+
+public class Chickens01 {
+    public static void main(String[] args) {
+        //Put yout code here
+    	Scanner sc = new Scanner (System.in);
+    	
+    	System.out.println("How many eggs does one chicken lay per day?");
+    	int eggsPerChicken = sc.nextInt();
+    	System.out.println("How many chickens do you have");
+    	int chickenCount = sc.nextInt();
+    	int totalEggs = 0;
+    	
+        //dag 1:
+    		totalEggs = eggsPerChicken * chickenCount;
+    		
+    	//dag 2:
+    		chickenCount ++;
+    		totalEggs = totalEggs + chickenCount * eggsPerChicken;
+    		
+    	//dag 3:
+    		chickenCount /=2.0 ;
+    		totalEggs = totalEggs + chickenCount * eggsPerChicken;
+    	
+    		sc.close();
+    		
+        System.out.println(totalEggs);
+    }   
+}
--- UserInput/src/input03/Input04.java
+++ UserInput/src/input03/Input04.java
@@ -7,16 +7,10 @@ public class Input04 {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner (System.in);
+				
+		int sum = 5 / 1;
 		
-		System.out.println("Enter max 5 nubmers to add up: ");
-		
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		int c = sc.nextInt();
-		int d = sc.nextInt();
-		int e = sc.nextInt();
-		
-		System.out.println((a+b+c+d+e));
+		System.out.println("Sum= " sum );
 	}
 
 }
